841aab43ba922479c669bc7080c6c4c8361d49ac
SILBIA <SILBIA@SILBIA>
2021-08-20 09:17:54 +0900

    21.08.20

---
+++ src/Ex6_6.java
@@ -0,0 +1,19 @@
+
+class Data{int x;}
+
+class Ex6_6 {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		chang(d.x);
+		System.out.println("After change(d.x)");
+		System.out.println("main() : x = " + d.x);
+	}
+
+	static void chang(int x) {
+		x = 1000;
+		System.out.println("change() : x = " + x);
+	}
+}
